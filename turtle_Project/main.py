a62a3f0bca2cb400afb67797b00572dc42a7fb26
Momin Taufeeq <147932981+MominTaufeeq@users.noreply.github.com>
2024-08-28 00:38:11 +0530

    Add files via upload

---
+++ turtle_Project/main.py
@@ -0,0 +1,25 @@
+import turtle
+
+vn=turtle.Screen()
+vn.setup(560,570)
+
+alex=turtle.Turtle()
+alex.shape('turtle')
+alex.color('red')
+
+alex.circle(50)
+alex.backward(100)
+alex.forward(200)
+alex.right(60)
+alex.left(60)
+
+alex.left(90)
+alex.forward(100)
+alex.left(90)
+alex.forward(200)
+alex.left(90)
+alex.forward(100)
+alex.left(90)
+alex.forward(200)
+alex.right(90)
+
